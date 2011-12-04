84fe7111091db8cd5e1929b1a2d829f955173eb4
Stephen Kapp <mort666@virus.org>
2011-12-04 13:39:48 +0000

    Fixed dumb cut and paste

--- lib/yubikey_database_authenticatable.rb
+++ lib/yubikey_database_authenticatable.rb
@@ -8,4 +8,4 @@ require 'devise_yubikey_database_authenticatable/model'
 require 'devise_yubikey_database_authenticatable/strategy'
 require 'devise_yubikey_database_authenticatable/routes'
 
-Devise.add_module(:Devise.add_module(:yubikey_database_authenticatable, :strategy => true, :model => "devise_yubikey_database_authenticatable/model", :route => :session, :controller => :sessions)
+Devise.add_module(:yubikey_database_authenticatable, :strategy => true, :model => "devise_yubikey_database_authenticatable/model", :route => :session, :controller => :sessions)
