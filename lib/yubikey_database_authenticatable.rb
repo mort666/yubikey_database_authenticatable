27753cc1f92e0bb34a49431c41069843ed5aad9f
Patrick Cable <pc@pcable.net>
2014-03-17 23:53:48 -0400

    Added support for local validation server, per #3

--- lib/yubikey_database_authenticatable.rb
+++ lib/yubikey_database_authenticatable.rb
@@ -24,6 +24,27 @@ module Devise
   #   config.yubikey_api_id = "" # => Api ID must be set to validate one time passwords
   mattr_accessor :yubikey_api_id
   @@yubikey_api_id = nil
+
+  # Public: The api_url for a yubikey validation endpoint
+  # If you're not using the Yubikey cloud, you'll need to set this. Otherwise,
+  # the YubiKey gem will take care of it.
+  # If you need this, set yubikey_api_url in the Devise configuration file
+  # (config/initializers/devise.rb).
+  #
+  #   config.yubikey_api_url = "" # => API URL of verifier
+  mattr_accessor :yubikey_api_url
+  @@yubikey_api_url = nil
+
+  # Public: The certificate_chain location for SSL validation
+  # If you're using your own verifier (you've specified yubikey_api_url) it's
+  # important that you pass the path to a verification chain for the CA or
+  # other certificates involved. If you need this, set
+  # yubikey_certificate_chain in the Devise configuration file
+  # (config/initializers/devise.rb).
+  #
+  #   config.yubikey_certificate_chain = "" # => API Cert Chain File
+  mattr_accessor :yubikey_certificate_chain
+  @@yubikey_certificate_chain = nil
 end
 
 Devise.add_module(:yubikey_database_authenticatable, :strategy => true, :model => "devise_yubikey_database_authenticatable/model", :route => :session, :controller => :sessions)
